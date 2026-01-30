a6b8dbe40fc62626de3e8dd8d1ec2f696612341c
perodaah <perodaah@gmail.com>
2026-01-30 03:13:33 +0100

    feat: register Poll, Option, and Vote models in Django admin

--- pollsystem/polls/admin.py
+++ pollsystem/polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Poll, Option, Vote
 
-# Register your models here.
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['title', 'is_active', 'created_by', 'created_at', 'expires_at']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['title', 'description']
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ['text', 'poll', 'order_index']
+    list_filter = ['poll']
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ['poll', 'option', 'voter_identifier', 'voted_at']
+    list_filter = ['poll', 'voted_at']
