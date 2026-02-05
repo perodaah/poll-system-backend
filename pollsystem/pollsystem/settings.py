d010fd757fae6be258912179d130cfdf51252be3
perodaah <perodaah@gmail.com>
2026-02-05 13:57:06 +0100

    deplpoy: fix issues with deployment on Railway and updated Ppollsystem/requirements.txt

--- pollsystem/pollsystem/settings.py
+++ pollsystem/pollsystem/settings.py
@@ -1,4 +1,9 @@
 from decouple import config
+from pathlib import Path
+from datetime import timedelta
+import os
+import dj_database_url
+
 """
 Django settings for pollsystem project.
 
@@ -11,7 +16,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('f=h7=a&uzs*hx%!i)*7je%*l5ckuqa52=$c@1h@!^%1@pg*xow')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -85,8 +90,6 @@ WSGI_APPLICATION = "pollsystem.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-import dj_database_url
-import os
 
 # Database Configuration
 # Railway provides DATABASE_URL automatically
@@ -175,8 +178,6 @@ SPECTACULAR_SETTINGS = {
     'COMPONENT_SPLIT_REQUEST': True,
 }
 
-# Production Settings
-import os
 
 # Only use these settings in production
 if not DEBUG:
